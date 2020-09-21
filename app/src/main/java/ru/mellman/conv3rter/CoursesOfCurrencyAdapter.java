9efb214740f870a4ae419f4c7200e9355b278395
EndyMellman <theunrealkosyak@gmail.com>
2020-09-22 01:05:35 +0700

    Updated version of application. New Design for ListViewItem

--- app/src/main/java/ru/mellman/conv3rter/CoursesOfCurrencyAdapter.java
+++ app/src/main/java/ru/mellman/conv3rter/CoursesOfCurrencyAdapter.java
@@ -2,16 +2,20 @@ package ru.mellman.conv3rter;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 
 import androidx.annotation.LayoutRes;
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.material.textview.MaterialTextView;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class CoursesOfCurrencyAdapter extends ArrayAdapter<CoursesOfCurrency> {
@@ -36,8 +40,11 @@ public class CoursesOfCurrencyAdapter extends ArrayAdapter<CoursesOfCurrency> {
 
             holder = new CoursesOfCurrencyHolder();
             holder.code = row.findViewById(R.id.code);
+            holder.currencyName = row.findViewById(R.id.currencyName);
             holder.currentValue = row.findViewById(R.id.valuevalute);
-            holder.previousValue = row.findViewById(R.id.previousvalue);
+            holder.difValue = row.findViewById(R.id.difvalue);
+            holder.nominal = row.findViewById(R.id.nominalvalute);
+            holder.diffenitionImage = row.findViewById(R.id.difImage);
             row.setTag(holder);
         }
         else{
@@ -45,15 +52,36 @@ public class CoursesOfCurrencyAdapter extends ArrayAdapter<CoursesOfCurrency> {
         }
         CoursesOfCurrency c = data.get(position);
         holder.code.setText(c.getCharCode());
-        holder.currentValue.setText(String.valueOf(c.getCourseValue()));
-        holder.previousValue.setText(String.valueOf(c.getPrevious()));
-
+        holder.currencyName.setText(c.getName());
+        String currentValueStr = String.valueOf(c.getCourseValue())+"₽";
+        holder.currentValue.setText(currentValueStr);
+        double dif = c.getCourseValue() - c.getPrevious();
+        if (dif>0){
+            holder.difValue.setTextColor(ContextCompat.getColor(context, R.color.courseGREEN));
+            holder.diffenitionImage.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_up));
+        }
+        if (dif<0){
+            holder.difValue.setTextColor(ContextCompat.getColor(context, R.color.courseRED));
+            holder.diffenitionImage.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_down));
+        }
+        if (dif==0.0){
+            holder.difValue.setTextColor(ContextCompat.getColor(context, R.color.courseGREY));
+        }
+        String difValue = new DecimalFormat("#0.0000").format(dif);
+        holder.difValue.setText(difValue);
+        String nominalStr = context.getString(R.string.nominal)+" "+c.getNominal()+c.getCharCode();
+        holder.nominal.setText(nominalStr);
         return row;
     }
     static class CoursesOfCurrencyHolder
     {
         MaterialTextView code;
+        MaterialTextView currencyName;
         MaterialTextView currentValue;
-        MaterialTextView previousValue;
+        MaterialTextView difValue;
+        MaterialTextView nominal;
+        ImageView diffenitionImage;
+
     }
+
 }
--- app/src/main/java/ru/mellman/conv3rter/MainActivity.java
+++ app/src/main/java/ru/mellman/conv3rter/MainActivity.java
@@ -4,11 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import android.content.Intent;
+
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -20,15 +19,11 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
 import android.widget.Toast;
 
 import com.google.android.material.appbar.MaterialToolbar;
-import com.google.android.material.snackbar.Snackbar;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -279,7 +274,6 @@ public class MainActivity extends AppCompatActivity {
             String text = txtValueFrom.getText().toString() + num;
             txtValueFrom.setText(text);
         }
-        calculateCourseBy(txtValueFrom,txtValueTo, selectedCurrencyPos);
     }
     private void InputDot(){
         int checkDot = txtValueFrom.getText().toString().indexOf(".");
